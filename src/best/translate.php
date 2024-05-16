1fca39762d340b8a50f61e3a4290a8457734138a
allahin <166995789+allahin@users.noreply.github.com>
2024-05-16 05:45:37 +0300

    new version

---
+++ src/best/translate.php
@@ -0,0 +1,33 @@
+<?php
+require 'vendor/autoload.php';
+
+use GeminiAPI\Client;
+use GeminiAPI\Resources\Parts\TextPart;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userInput = $_POST['inputText'];
+
+    if(empty($userInput)){
+        echo '<div class="alert alert-danger" role="alert">Lütfen metin girişi yapın!</div>';
+    } else {
+        $client = new Client('AIzaSyDCJerCvcr0-jAgcJO4jGxEqSszOAc8IxY');
+        $response = $client->geminiPro()->generateContent(
+            new TextPart('Gireceğim metnin dilini yaz, çıktı olarak sadece metnin dilini yaz. "'. $userInput .'"'),
+        );
+
+        echo '<div class="yeah" role="alert">Girdiğiniz metnin dili: ' . $response->text() . '</div>';
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userInput = $_POST['inputText'];
+
+    if(empty($userInput)){
+    } else {
+        $client = new Client('AIzaSyDCJerCvcr0-jAgcJO4jGxEqSszOAc8IxY');
+        $response = $client->geminiPro()->generateContent(
+            new TextPart('Gireceğim metni türkçeye çevir, çıktı olarak sadece çeviriyi yaz. "'. $userInput .'"'),
+        );
+        echo '<div class="alert alert-success" role="alert">' . $response->text() . '</div>';
+    }
+}
